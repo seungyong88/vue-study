184283003fc6bac7d39c53afe0bf45e008aefa7c
sygo <seungyong@saltlux.com>
2021-02-19 10:17:13 +0900

    Router Study

---
+++ vue-bootstrap/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>Test</div>
+   <!-- <Test /> -->
+</template>
+
+<script>
+import Test from "./components/Test.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
